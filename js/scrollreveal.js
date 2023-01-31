66eb0acd83b6a885373023ed79cb4817d9757912
Rafael Blezins <rafablezins@gmail.com>
2023-01-30 23:32:56 -0300

    Primeiro commit

---
+++ js/scrollreveal.js
@@ -0,0 +1,43 @@
+// Animations config
+
+ScrollReveal({reset: true})
+
+// Text - Home
+ScrollReveal().reveal('.text-container', {
+    delay: 300,
+    origin: 'left',
+    distance: '150px',
+    duration: 500
+})
+
+// Ilustração - Sobre
+ScrollReveal().reveal('.illustration', {
+    delay: 300,
+    origin: 'left',
+    distance: '150px',
+    duration: 500
+})
+
+// Descrição - Sobre
+ScrollReveal().reveal('.description', {
+    delay: 300,
+    origin: 'right',
+    distance: '150px',
+    duration: 500
+})
+
+// Serviços
+ScrollReveal().reveal('.service', {
+    delay: 300,
+    origin: 'bottom',
+    distance: '300px',
+    duration: 800
+})
+
+// Cards - Projetos
+ScrollReveal().reveal('.card', {
+    delay: 300,
+    origin: 'bottom',
+    distance: '150px',
+    duration: 800
+})
---
+++ js/typed.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-type", {
+    strings: ["Sou designer e programador front-end. Ajudo pessoas e empresas a construírem seus projetos de maneira moderna e com um design de alta qualidade."],
+    startDelay: 1000,
+    typeSpeed: 50,
+    loop: false
+})
